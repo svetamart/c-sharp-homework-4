b8a7f45aa372a4b2f999f853784b06f4ce77c9c5
Sveta Martynova <svetamart95@gmail.com>
2022-09-08 22:43:35 +0300

    added task 41

---
+++ hw6/task 41/Program.cs
@@ -0,0 +1,53 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите длину массива");
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+if(!isNumber)
+{
+    Console.WriteLine("Пожалуйста, введите корректное число");
+    return;
+}
+
+int[] array = new int[number];
+if(array.Length <= 0)
+    {
+        Console.WriteLine("Длина массива не может быть отрицательным числом или равняться нулю");
+        return;
+    }
+
+Console.WriteLine("Введите значения, которыми вы хотите заполнить массив");
+
+
+for (int i = 0; i < array.Length; i++)
+array[i] = int.Parse(Console.ReadLine());
+
+void PrintArray (int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+PrintArray(array);
+
+
+int FindPositiveNumbers (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine();
+
+int outcome = FindPositiveNumbers(array);
+Console.WriteLine(outcome);
