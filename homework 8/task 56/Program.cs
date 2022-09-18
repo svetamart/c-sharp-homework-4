81eaa5e7327c98544505abcd0fe9426e01f59576
Sveta Martynova <svetamart95@gmail.com>
2022-09-18 23:49:07 +0300

    added homework 8

---
+++ homework 8/task 56/Program.cs
@@ -0,0 +1,103 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите количество строк");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+
+Console.WriteLine("Введите количество столбцов");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isNumberM || m <= 0 || !isNumberN || n <= 0)
+{
+    Console.WriteLine("Одно или оба числа введены неверно");
+    return;
+}
+
+if (m == n)
+{
+    Console.WriteLine("Пожалуйста, задайте прямоугольный массив");
+    return;
+}
+
+int[,] result = FillArray(m, n);
+Print2DArray(result);
+Console.WriteLine();
+int[] sum = FindRowSum(result, m, n);
+PrintArray(sum);
+int min = FindMinRow(sum);
+Console.WriteLine($"Строка с наименьшей суммой элементов: {min} строка");
+
+
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[] FindRowSum(int[,] array, int m, int n)
+{
+    int[] sum = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            sum[i] = sum[i] + array[i, j];
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+int FindMinRow(int[] array)
+{
+    int min = array[0];
+    int rowNumber = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+            rowNumber = i + 1;
+        }
+    }
+    return rowNumber;
+}
